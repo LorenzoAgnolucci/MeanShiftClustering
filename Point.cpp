ca4e8ed17fd18370e99938fba737fa8facf66821
Lorenzo Agnolucci <lory.agnolucci@gmail.com>
2019-12-27 17:04:46 +0100

    Implemented operators

--- Point.cpp
+++ Point.cpp
@@ -3,3 +3,37 @@
 //
 
 #include "Point.h"
+
+
+Point &Point::operator+=(const Point &otherPoint){
+    for (int i = 0; i < this->getNumDimensions(); ++i) {
+        this->values[i] += otherPoint.getValues()[i];
+
+    }
+    return *this;
+}
+
+Point Point::operator*(const float d) const {
+    Point point(this->values);
+    return point *= d;
+}
+
+
+Point &Point::operator*=(const float d) {
+    for (long i = 0; i < getNumDimensions(); ++i)
+        this->values[i] *= d;
+    return *this;
+}
+
+
+Point Point::operator/(const float d) const {
+    Point point(this->values);
+    return point /= d;
+}
+
+
+Point &Point::operator/=(const float d) {
+    for (long i = 0; i < getNumDimensions(); ++i)
+        this->values[i] /= d;
+    return *this;
+}
--- Point.h
+++ Point.h
@@ -2,16 +2,23 @@
 // Created by lorenzo on 19/12/19.
 //
 
-#ifndef MIDTERM_PARALLEL_COMPUTING_POINT_H
-#define MIDTERM_PARALLEL_COMPUTING_POINT_H
+#ifndef MEANSHIFTCLUSTERING_POINT_H
+#define MEANSHIFTCLUSTERING_POINT_H
 
+#include <utility>
 #include <vector>
 
 class Point {
 
 public:
+    explicit Point(std::vector<float> values) : values(std::move(values)) {}
 
-    int getNumDimensions(){
+
+    explicit Point(const int numDimensions){
+        this->values = std::vector<float>(numDimensions, 0);
+    }
+
+    int getNumDimensions() const {
         return values.size();
     }
 
@@ -23,10 +30,20 @@ public:
         Point::values = newValues;
     }
 
+    Point &operator+=(const Point &otherPoint);
+
+    Point operator*(float d) const;
+
+    Point &operator*=(float d);
+
+    Point operator/(float d) const;
+
+    Point &operator/=(float d);
+
 private:
 
     std::vector<float> values;
 };
 
 
-#endif //MIDTERM_PARALLEL_COMPUTING_POINT_H
+#endif //MEANSHIFTCLUSTERING_POINT_H
