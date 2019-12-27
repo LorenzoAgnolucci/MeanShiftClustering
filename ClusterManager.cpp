e9b5fef76135bd3619699c219ff55a11af13d4af
Lorenzo Agnolucci <lory.agnolucci@gmail.com>
2019-12-27 19:00:04 +0100

    Added files

---
+++ ClusterManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lorenzo on 27/12/19.
+//
+
+#include "ClusterManager.h"
---
+++ ClusterManager.h
@@ -0,0 +1,14 @@
+//
+// Created by lorenzo on 27/12/19.
+//
+
+#ifndef MEANSHIFTCLUSTERING_CLUSTERMANAGER_H
+#define MEANSHIFTCLUSTERING_CLUSTERMANAGER_H
+
+
+class ClusterManager {
+
+};
+
+
+#endif //MEANSHIFTCLUSTERING_CLUSTERMANAGER_H
