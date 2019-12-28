89ab1d2df0d6f60bcb87872d570b31b441e462cd
Lorenzo Agnolucci <lory.agnolucci@gmail.com>
2019-12-28 11:29:06 +0100

    Created basic class

--- Cluster.h
+++ Cluster.h
@@ -6,8 +6,33 @@
 #define MEANSHIFTCLUSTERING_CLUSTER_H
 
 
+#include <vector>
+
+#include "Point.h"
+
 class Cluster {
 
+public:
+
+    explicit Cluster(Point &centroid) {
+        this->centroid = std::move(centroid);
+    }
+
+    void addPoint(const Point &point){
+        points.push_back(point);
+    }
+
+    const std::vector<Point> &getPoints() const {
+        return points;
+    }
+
+    const Point &getCentroid() const {
+        return centroid;
+    }
+
+private:
+    std::vector<Point> points;
+    Point centroid;
 };
 
 
