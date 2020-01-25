bed45a05dba6589f6b3870fb4c8deb8f8ff2743e
Lorenzo Agnolucci <lory.agnolucci@gmail.com>
2020-01-25 14:34:40 +0100

    Implemented the function to generate datasets

---
+++ generate_datasets.py
@@ -0,0 +1,55 @@
+import os
+
+import sklearn.datasets as datasets
+import pandas as pd
+
+
+DATASETS_DIR = 'datasets/'
+
+
+def generate_dataset(points, n_features, centers, std, file_name):
+    data, __ = datasets.make_blobs(
+        points, n_features, centers, cluster_std=std, shuffle=True,
+        random_state=1000
+    )
+    pd.DataFrame(data).to_csv(
+        os.path.join(DATASETS_DIR, file_name), header=False, index=False
+    )
+
+
+def main():
+
+    dataset_num = 1
+
+    #######################################################
+    #### Datasets with std=1 and well defined clusters ####
+    #######################################################
+
+    # two dimensional datasets
+    for c in range(1, 6):
+        generate_dataset(
+            points=10000, n_features=2, centers=c, std=1,
+            file_name=f'data{dataset_num}.csv'
+        )
+        dataset_num += 1
+    # three dimensional datasets
+    for c in range(1, 6):
+        generate_dataset(
+            points=10000, n_features=3, centers=c, std=1,
+            file_name=f'data{dataset_num}.csv'
+        )
+        dataset_num += 1
+
+    #######################################################
+    ### Datasets with an increasing number of points ######
+    #######################################################
+
+    for points in [100, 1000, 10000, 20000, 50000]:
+        generate_dataset(
+            points, n_features=3, centers=5, std=1,
+            file_name=f'data{dataset_num}.csv'
+        )
+        dataset_num += 1
+
+if __name__ == '__main__':
+    main()
